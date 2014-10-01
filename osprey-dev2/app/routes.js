954964f2940edd9add02d45f54544426979e3ae6
Patrick Emami <patrickemami@Patricks-MacBook-Air-2.local>
2014-09-30 22:24:20 -0400

    Adding new osprey-dev2 folder

---
+++ osprey-dev2/app/routes.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var UserModel = require('./models/user');
+
+module.exports = function(app) {
+	// api ---------------------------------------------------------------------
+
+	app.post('/auth/login', function(req, res) {
+		// use mongoose to get all todos in the database
+		UserModel.findOne({email : req.body.email, password : req.body.password}, function(err, users) {
+			// if there is an error retrieving, send the error. nothing after res.send(err) will execute
+			if (err)
+				res.send(err);
+			res.json(users); // return all todos in JSON format
+		});
+	});
+
+	// // create todo and send back all todos after creation
+	app.post('/auth/register', function(req, res) {
+
+		// create a todo, information comes from AJAX request from Angular
+		UserModel.create({
+			email : req.body.email, 
+			password : req.body.password,
+			done : false
+		}, function(err, user) {
+			if (err)
+				res.send(err);
+
+			// get and return all the todos after you create another
+			UserModel.find(function(err, user) {
+				if (err)
+					res.send(err);
+				res.send(user);
+			});
+		});
+
+	});
+	
+	app.post('/auth/checkReg', function(req, res) {
+		// use mongoose to get all todos in the database
+		UserModel.count({email : req.body.email}, function(err, count) {
+			// if there is an error retrieving, send the error. nothing after res.send(err) will execute
+			if (err)
+				res.send(err);
+			res.json(count); // return all todos in JSON format
+		});
+	});
+
+	// application -------------------------------------------------------------
+	// app.get('*', function(req, res) {
+	// 	res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+	// });
+
+};
