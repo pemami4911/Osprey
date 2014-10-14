3689c3f16668a6f35afb2b707420a21be208108b
njiang1209 <njiang1209@ufl.edu>
2014-10-14 12:55:44 -0400

    table changes

--- osprey-dev2/public/js/controllers/dashboardMain.js
+++ osprey-dev2/public/js/controllers/dashboardMain.js
@@ -1,13 +1,84 @@
 'use strict';
-angular.module('dashboardPageModule', ['splashPageService'])
-
-	
+angular.module('dashboardPageModule', ['splashPageService', 'ngReactGrid'])
 
 	// inject the Todo service factory into our controller
-	.controller('dashboardController', ['$scope','$http', '$location', 'splashFactory', function($scope, $http, $location, splashFactory) {
+	.controller('dashboardController', ['$scope',
+										'$http', 
+										'$location', 
+										'splashFactory', 
+										'ngReactGridCheckbox', 
+	function($scope, $http, $location, splashFactory, ngReactGridCheckbox) {
 		$scope.activeTab = 1;
 		$scope.contentUrl = 'views/dashPartials/dashMain.html';
 		$scope.loggedUser = {};
+
+		$scope.tableSelections = [];
+		$scope.tableData = [
+			{	
+				"patientName" : "John Smith",
+			 	"parentName" : "Eric Smith",
+			 	"email" : "john@yahoo.com",
+			 	"weight" : 110,
+			 	"age" : 7
+			},
+			{	
+				"patientName" : "Jane Smith",
+			 	"parentName" : "abc def",
+			 	"email" : "test@yahoo.com",
+			 	"weight" : 123,
+			 	"age" : 6
+			},
+			{	
+				"patientName" : "das asda",
+			 	"parentName" : "sdf sdfs",
+			 	"email" : "as@yahoo.com",
+			 	"weight" : 80,
+			 	"age" : 5
+			}
+		];
+
+		for (var i = 1; i <= 100; i++) {
+			$scope.tableData.push({	
+				"patientName" : "Patient " + i,
+			 	"parentName" : "Parent " + i,
+			 	"email" : "test@email.com" + i,
+			 	"weight" : i,
+			 	"age" : i * 2
+			})
+		}
+        
+        $scope.grid = {
+                data: $scope.tableData,
+                columnDefs: [
+                new ngReactGridCheckbox($scope.tableSelections),
+                {
+                    field: "patientName",
+                    displayName: "Patient Name",
+                    render: function(row) {
+                      return React.DOM.a({href:"javascript:", onClick: function() {
+                          console.log(row);
+                      }}, row.patientName);
+                  	}
+                },
+                {
+                    field: "parentName",
+                    displayName: "Parent Name"
+                },
+                {
+                    field: "email",
+                    displayName: "E-mail Address"
+                },
+                {
+                    field: "weight",
+                    displayName: "Weight"
+                },
+                {
+                    field: "age",
+                    displayName: "Age"
+                }]
+        };
+
+
 		splashFactory.isLoggedIn()
 			// if successful creation, call our get function to get all the new todos
 			.success(function(data) {
