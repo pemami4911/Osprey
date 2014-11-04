caa1efb06b9a3a80082e84bd87646a33ea27df83
Patrick Emami <patrickemami@gmail.com>
2014-11-04 17:24:00 -0500

    Added back button to registration forms, fixed bugs with routing changes

--- osprey-dev2/public/js/controllers/regPhysicianMain.js
+++ osprey-dev2/public/js/controllers/regPhysicianMain.js
@@ -8,24 +8,38 @@ angular.module('regPhysicianPageModule', ['splashPageService'])
 		$scope.regData.userType = splashFactory.get('userType'); 
 		$scope.loading = false;
 
+		// $scope.$on is an event handler
+		// $routeChangeStart is an angular event that is called every time a route change begins
+		$scope.$on('$locationChangeStart', function (event) {
+	        if( !($scope.loading) )
+	        	event.preventDefault();
+   		})
+
 		$scope.registerFinal = function() {
 			$scope.loading = true;
-
 			// validate the formData to make sure that something is there
 			// if form is empty, nothing will happen
 			if ($scope.regData.email != undefined) {
-
 				// call the create function from our service (returns a promise object)
 				splashFactory.registerFinal($scope.regData)
 					// if successful creation, call our get function to get all the new todos
 					.success(function(data) {
-						// console.log(data);
+						//console.log(data);
 						//window.alert("User created");
-						$location.path('/dashboard');
+						if( data != 'null' )
+							$location.path('/dashboard');
+						else
+							window.alert("Failed to register!");
 					}).error(function(response) {
 						$scope.error = response.message;
+						$scope.loading = false;
 					});
 			}
-			$scope.loading = false; 
+			else
+				$scope.loading = false; 
 		};
+
+		$scope.back = function() {
+			$location.path('/'); 
+		}
 	}]);
