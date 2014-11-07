7372d0f6b8bf088aec61edf69244b91ed0d62715
njiang1209 <njiang1209@ufl.edu>
2014-11-06 23:02:26 -0500

    more grunt stuff

--- osprey-dev2/gruntfile.js
+++ osprey-dev2/gruntfile.js
@@ -60,6 +60,28 @@ module.exports = function(grunt) {
 		unit: {
 		    configFile: 'karma.conf.js'
 		}
+	    },
+	    protractor: {
+	    	options: {
+		      configFile: "protractor.conf.js" // Default config file
+
+		    },
+		    all: {}
+	    },
+	    protractor_webdriver: {
+		    your_target: {
+		      options: {
+		        command: 'webdriver-manager start'
+		      },
+		    },
+		},
+		nodemon: {
+		    dev: {
+		    	script: 'server.js',
+		    	options: {
+		    		args: ['NODE_ENV=test']
+		    	}
+		    }
 	    }
 	});
 
@@ -71,8 +93,12 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-mocha-test');
     grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-env');
+    grunt.loadNpmTasks('grunt-protractor-runner');
+    grunt.loadNpmTasks('grunt-protractor-webdriver');
+    grunt.loadNpmTasks('grunt-nodemon');
 
-    grunt.registerTask('test', ['env:test', 'karma', 'mochaTest']);
+    grunt.registerTask('test', ['env:test', 'karma', 'mochaTest', 'protractor_webdriver', 'protractor']);
+    grunt.registerTask('default', ['nodemon']);
    	grunt.registerTask('testmocha', ['mochaTest']);
     // grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
 
