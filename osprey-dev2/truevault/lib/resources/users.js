8d4e3b51394b329f8a9972bc7945c0e5b72f9cd0
njiang1209 <njiang1209@ufl.edu>
2014-11-17 16:34:39 -0500

    wip

---
+++ osprey-dev2/truevault/lib/resources/users.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var Resource = require('../resource');
+var util = require('util');
+
+function Users() {
+  Users.super_.apply(this, arguments);
+}
+
+util.inherits(Users, Resource);
+
+
+// Retrieves all users
+//
+// callback is optional, this method returns a q promise
+Users.prototype.list = function(callback) {
+
+  var path = util.format("/%s/users",
+    this.truevault.getOption('api_version')
+  );
+
+  return this.httpsRequest({
+    path : path,
+    method : 'GET',
+    callback : callback
+  });
+
+};
+
+// Creates a user
+//
+// options.vault_id - vault uuid
+// options.schema - a javascript object
+// callback is optional, this method returns a q promise
+Users.prototype.create = function(options, callback) {
+  var path = util.format("/%s/users",
+    this.truevault.getOption('api_version')
+  );
+  var attributes = JSON.stringify(options.attributes);
+  var encodedAttributes = new Buffer(attributes, 'utf8').toString('base64');
+  var data = {
+    attributes : encodedAttributes,
+    username : options.username,
+    password: options.password
+  };
+
+  return this.httpsRequest({
+    path : path,
+    method : 'POST',
+    data : data,
+    callback : callback
+  });
+
+};
+
+
+
+module.exports = Users;
