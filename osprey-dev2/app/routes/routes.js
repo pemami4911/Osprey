cfbbb578497316c57d2dc5d43ca1eb7900b3fc91
njiang1209 <njiang1209@ufl.edu>
2014-11-25 11:09:28 -0500

    factor out users

--- osprey-dev2/app/routes/routes.js
+++ osprey-dev2/app/routes/routes.js
@@ -33,6 +33,8 @@ var AuthModule = require('./auth');
 var Auth = new AuthModule(globals, api_key, vaultid);
 var SettingsModule = require('./settings'); 
 var Settings = new SettingsModule(globals, api_key, vaultid); 
+var UsersModule = require('./users'); 
+var Users = new UsersModule(globals, api_key, vaultid); 
 
 // -----------------------------------------------------------------------------
 
@@ -83,92 +85,15 @@ module.exports = function(app) {
 	});
 
 	app.get('/users/unassignedParents', function(req, res) {
-	// 	if (!req.isAuthenticated())
-	// 		res.send(false);
-		
-	// 	UserModel.find({ $and: [{userType : "Parent"}, {physician: null}] }, function(err, data) {
-	// 		console.log(data);
-	// 		res.send(data);
-	// 	});
+		Users.unassignedParents(req, res);
 	 });
 
 	app.post('/users/childrenOfParent', function(req, res) {
-		var temp = require('../../truevault/lib/truevault.js')(req.session.access_token);
-		temp.auth.verify(function(err, value){
-			console.log("verified");
-			if (err) {
-				console.log("verification error");
-				res.send(false);
-			} else {
-				// find user attributes
-				var options = {
-					'vault_id' : vaultid,
-					'schema_id' : globals.childSchemaId,
-				  	'filter' : { 
-				  		'parentId': {
-					    	"type": "eq",
-					    	"value": req.body.parentId
-					    }
-					},
-					'full_document' : true
-				};
-				truevault.documents.search(options, function (err2, value2) {
-					if (err) {
-						console.log('search error');
-						res.send(err2);
-					}
-					else {
-						if (value2.data.documents.length == 0)
-							console.log("no matching child documents found");
-						else {
-							var retObject = {"content":[]};
-							for (var i = 0; i < value2.data.documents.length; i++) {
-								addChild(res, retObject, value2.data.documents.length, value2.data.documents[i].document_id);
-							}
-						}
-					}
-				});
-			}
-		});
+		Users.childrenOfParent(req, res);
 	});
 
-	// helper function for /users/childrenOfParent
-	// adds children to an array, sends if all children have been found and added to array
-	var addChild = function(res, retObject, numChildren, childId) {
-		truevault.documents.retrieve({
-		   'vault_id' : vaultid,
-		   'id' : childId
-		}, function (err, document){
-			retObject.content.push(document);
-			if (retObject.content.length == numChildren) {
-				console.log(retObject);
-				res.json(retObject);
-			}
-		});
-	}
-
 	app.post('/users/childrenOfPhysician', function(req, res) {
-		var temp = require('../truevault/lib/truevault.js')(req.session.access_token);
-		temp.auth.verify(function(err, value){
-			console.log("verified");
-			if (err) {
-				console.log("verification error");
-				res.send(false);
-			} else {
-				// find user attributes
-				var options = {
-					'vault_id' : vaultid,
-					'schema_id' : globals.childSchemaId,
-				  	'filter' : { 
-				  		'parentId': {
-					    	"type": "eq",
-					    	"value": req.body.parentId
-					    }
-					},
-					'full_document' : true
-				};
-			}
-		});
+		Users.childrenOfPhysician(req, res);
 	});
 
 	app.get('/nvd3css', function(req, res){
